72efbecebca5297e7f7b5e759f1114b61b1f0a60
Dionatan <dionataninhoato@gmail.com>
2025-04-08 11:34:32 -0300

    Botão de contraste e javascript

--- script.js
+++ script.js
@@ -13,4 +13,18 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoFonte -= 0.1;
         document.body.style.fontSize = tamanhoFonte + 'rem';
     })
+
+    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoAcessibilidade.addEventListener('click', function(){
+        botaoAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesAcessibilidade.classList.toggle('apresenta-lista');
+    })
+
+    const alternaContraste = document.getElementById('alterna-contraste')
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+})
 })
